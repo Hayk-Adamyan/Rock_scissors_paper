6292653c59364c51bba860716b16f54a1c9920cf
Hayk.Adamyan <53045573+Hayk-Adamyan@users.noreply.github.com>
2022-02-28 17:54:36 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [stateUser, setStateUser] = useState()
+    const [stateComp, setStateComp] = useState()
+    const [result, setResult] = useState()
+    const choyses = ["qar", "tuxt", "mkrat"]
+
+    const compChoise = () => {
+        const random = choyses[Math.floor(Math.random() * choyses.length)]
+        setStateComp(random)
+
+    }
+    const handleClick = (val) => {
+        setStateUser(val)
+        compChoise()
+
+    }
+    useEffect(()=>{
+        gameResult()
+    },[stateComp,stateUser])
+
+
+    const gameResult = () => {
+        // eslint-disable-next-line default-case
+        switch (stateUser + stateComp) {
+            case "qarmkrat":
+            case "mkrattuxt":
+            case "tuxtqar":
+                setResult("YOU WIN")
+                break
+            case "qartuxt":
+            case "mkratqar":
+            case "tuxtmkrat":
+                setResult("YOU LOSE")
+                break
+            case "qarqar":
+            case "tuxttuxt":
+            case "mkratmkrat":
+                setResult("DRAW")
+                break
+
+        }
+
+    }
+    return (
+        <div>
+            <h1>user:{stateUser}</h1>
+            <h1>computer:{stateComp}</h1>
+            {choyses.map((choyse, index) => <button key={index} onClick={() => handleClick(choyse)}>{choyse}</button>)}
+            <h1>{result}</h1>
+
+
+        </div>
+    );
+};
 
 export default App;
